dcfc8b7781d48ecaf09ac7a2984ec2f9d4aa409d
Krzysiek <kgadek01@email.com>
2021-05-06 14:55:00 +0200

    -dodanie kolejnych wspozednych

--- logic_java/src/logic_java/Main.java
+++ logic_java/src/logic_java/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Punkt punkt = new Punkt(10, 20);
-		System.out.println("Wspó³rzêdne to: ");
-		System.out.println("x: " + punkt.x);
-		System.out.println("y: " + punkt.y);
+		Punkt punkt1 = new Punkt(10, 20);
+ 		System.out.println("Wspó³rzêdne 1 to: ");
+ 		System.out.println("x1: "+punkt1.x);
+ 		System.out.println("y2: "+punkt1.y);
+ 
+ 		Punkt punkt2 = new Punkt(punkt1);
+ 		System.out.println("Wspó³rzêdne 2 to: ");
+ 		System.out.println("x2: "+punkt2.x);
+ 		System.out.println("y2: "+punkt2.y);
 
 		int a = 5;
 		Test.zwieksz(a);
