d367863e5042753dba2e120082cc64e0a3421b35
Szymon <szymon.baranski@zse.krakow.pl>
2021-05-06 14:29:41 +0200

    - dodanie dwóch metod sumujące dwie zmienne

--- logic_java/src/logic_java/Test.java
+++ logic_java/src/logic_java/Test.java
@@ -1,11 +1,22 @@
 package logic_java;
 
 class Test {
+	
 	static void zwieksz(int liczba) {
 		liczba++;
 	}
+	
 	static void zmien(Punkty pkt) {
 		pkt.wspX++;
 		pkt.wspY++;
 	}
+	
+	int dodaj(int a, int b) {
+		return a+b;
+	}
+	
+	double dodaj(double a, double b) {
+		return a+b;
+	}
+	
 }
