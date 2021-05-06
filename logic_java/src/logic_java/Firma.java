554666e5a2535e2e88e10c74436e1a1c0b37a920
Krzysiek <kgadek01@email.com>
2021-05-06 15:05:45 +0200

    -tworzenie klasy firma i wprowadzenie danych pracownika

---
+++ logic_java/src/logic_java/Firma.java
@@ -0,0 +1,28 @@
+package logic_java;
+
+public class Firma {
+
+	public static void main(String args[]) {
+		Pracownik prac = new Pracownik("Wlodek", "Ziêba", 3000);
+		System.out.println("Imiê: " + prac.imie);
+		System.out.println("Nazwisko: " + prac.nazwisko);
+		System.out.println("Wyp³ata: " + prac.wyplata + "\n");
+
+		Szef szef = new Szef();
+
+		System.out.println("Imiê: " + szef.imie);
+		System.out.println("Nazwisko: " + szef.nazwisko);
+		System.out.println("Wyp³ata: " + szef.wyplata);
+		System.out.println("Premia: " + szef.premia + "\n");
+
+		szef.imie = "Tadeusz";
+		szef.nazwisko = "Kowalski";
+		szef.wyplata = 10000;
+		szef.premia = 2000;
+		System.out.println("Imiê: " + szef.imie);
+		System.out.println("Nazwisko: " + szef.nazwisko);
+		System.out.println("Wyp³ata: " + szef.wyplata);
+		System.out.println("Premia: " + szef.premia);
+	}
+
+}
