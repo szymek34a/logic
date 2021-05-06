0df61819d8a2693b8b0740d7f0ec5125e5dd3b4d
Szymon <szymon.baranski@zse.krakow.pl>
2021-05-06 15:01:42 +0200

    - stworzenie klasy Pracownik oraz metod w ktorych przechowujemy zmienne pracownikow

---
+++ logic_java/src/logic_java/Pracownik.java
@@ -0,0 +1,19 @@
+package logic_java;
+
+class Pracownik {
+	String imie;
+	String nazwisko;
+	int wyplata;
+	
+	public Pracownik() {
+		imie = "";
+		nazwisko = "";
+		wyplata = 0;
+	}
+	
+	public Pracownik(String i, String n, int w) {
+		imie = i;
+		nazwisko = n;
+		wyplata = w;
+	}
+}
