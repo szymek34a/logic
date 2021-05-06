ae5d41e7160f8749810096845e6586f67f494dc5
Szymon <szymon.baranski@zse.krakow.pl>
2021-05-06 14:09:09 +0200

    - dodanie trzech metod, pierwsz wyświetla napis i go zwraca, pozostałe sumują liczby

--- logic_java/src/logic_java/Main.java
+++ logic_java/src/logic_java/Main.java
@@ -9,17 +9,30 @@ public class Main {
 		punkty.ustawY(20);
 		System.out.println("Wspó³rzêdne punktu to (" + punkty.dajX() + ", " + punkty.dajY() + ")");
 	}
-	
+
 	void metoda1() {
 		System.out.println("Ta metoda nic nie zwraca ale wyœwietla ten tekst");
 	}
-	
+
 	int metoda2() {
 		return 2;
 	}
-	
+
 	String metoda3() {
 		return "Jakiœ napis";
 	}
-	
+
+	String metodaNapis(String str) {
+		System.out.println(str);
+		return str;
+	}
+
+	int sumaLiczb(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	double sumaLiczb2(int a, short b, double c) {
+		return a + b + c;
+	}
+
 }
