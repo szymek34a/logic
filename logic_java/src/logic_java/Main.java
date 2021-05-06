d9376008329ce3d02eaad2e44e8d8bd3f376e5a1
Krzysiek <kgadek01@email.com>
2021-05-06 13:59:47 +0200

    - sprawdzenie utworzonych metod

--- logic_java/src/logic_java/Main.java
+++ logic_java/src/logic_java/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Punkt punkt = new Punkt();
- 		punkt.wspX = 10;
- 		punkt.wspY = 20;
- 		System.out.println("Wspó³rzêdne punktu to ("+punkt.wspX+", "+punkt.wspY+")");
+		punkt.ustawX(10);
+ 		punkt.ustawY(20);
+ 		System.out.println("Wspó³rzêdne to: ("+ punkt.dajX() + ", "+ punkt.dajY() +")");
 		
 	}
 
